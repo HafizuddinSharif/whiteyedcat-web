4059b4c19bab06407b18c19b2d31ca115fc12a45
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2026-05-03 13:27:06 +0800

    feat: add open graph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: process.env.SITE ?? process.env.PUBLIC_SITE_URL ?? process.env.CF_PAGES_URL ?? 'https://whiteyedcat.pages.dev',
+});
